2be0519831ce252d97245271eb8531381a40df4b
Magesh Kuppan <tkmagesh77@gmail.com>
2021-05-13 17:21:52 +0530

    Update bug-tracker.component.spec.ts

--- bug-tracker-app/src/app/bug-tracker/bug-tracker.component.spec.ts
+++ bug-tracker-app/src/app/bug-tracker/bug-tracker.component.spec.ts
@@ -7,6 +7,7 @@ import { bugs as mockbugs } from './mock-data/bugs';
 import { Bug } from './models/bug';
 import { BugEditComponent } from './components/bug-edit.component';
 import { By } from '@angular/platform-browser';
+import { BugStatsComponent } from './components/bug-stats.component';
 
 fdescribe('BugTrackerComponent', () => {
   let component: BugTrackerComponent;
@@ -59,6 +60,16 @@ fdescribe('BugTrackerComponent', () => {
     const bugEditComponent : BugEditComponent = el.query(By.directive(BugEditComponent)).componentInstance;
     bugEditComponent.created.emit({ id : 100, name : 'Dummy Bug', isClosed : false, createdAt : new Date()});
     expect(component.bugs.length).toBe(1);
+  })
+
+  it('should pass the bugs as input to the bugstats component', () => {
+    let bugOperationSpy : BugOperationsService = TestBed.inject(BugOperationsService);
+    spyOn(bugOperationSpy, "getAll").and.returnValue(of(mockbugs));
+    component.ngOnInit();
+    fixture.detectChanges();
+    const el = fixture.debugElement;
+    const bugstatsComponent : BugStatsComponent = el.query(By.directive(BugStatsComponent)).componentInstance;
+    expect(bugstatsComponent.bugs.length).toBe(mockbugs.length);
   })
 
 });
